f5a750c6d36e5ad49d5a1d6055abf81858225336
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-01-16 21:27:20 +0000

    Adding favicon

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route("/blog")
+def blog():
+    return "<p>My blog!</p>"
