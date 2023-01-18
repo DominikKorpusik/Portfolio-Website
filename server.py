00a730734e70cac6d9dd0a9ab9c53b949349298d
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-01-18 23:20:23 +0000

    Changing work.html

--- server.py
+++ server.py
@@ -1,15 +1,13 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-@app.route("/blog")
-def blog():
-    return "<p>My blog!</p>"
